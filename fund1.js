6cb7dd85c51dd126eb1f3cef2e945daf0284e0c1
ZanaSiaka <zanacoul210@gmail.com>
2024-05-07 02:29:51 +0200

    Found 1 (Coercion)

---
+++ fund1.js
@@ -0,0 +1,32 @@
+//Coding Challenge 1 
+// shift + alt + A
+
+/* 
+let markMass = 95
+let markHeight = 1.88
+let johnMass = 85
+let johnHeight = 1.76
+
+const markBMI = markMass / markHeight ** 2
+const johnBMI = johnMass / johnHeight ** 2
+
+const markHeigherBMI = markBMI > johnBMI
+
+console.log(markBMI, johnBMI, markHeigherBMI) */
+
+
+// Coding Challenge 2
+/* 
+let markMass = 95
+let markHeight = 1.88
+let johnMass = 85
+let johnHeight = 1.76
+
+const markBMI = markMass / markHeight ** 2
+const johnBMI = johnMass / johnHeight ** 2
+
+if (markBMI > johnBMI){
+    console.log(`Mark's BMI (${markBMI}) is heigher than John's (${johnBMI})`)
+} else {
+    console.log(`John's BMI (${johnBMI}) is heigher than Mark's (${markBMI})`)
+} */
