a877f6e1797dc02d5ab6051ffa4cb7f0494daf38
ZanaSiaka <zanacoul210@gmail.com>
2024-05-08 08:18:20 +0200

    Found 1 (Coding challenge 3)

--- fund1.js
+++ fund1.js
@@ -30,3 +30,33 @@ if (markBMI > johnBMI){
 } else {
     console.log(`John's BMI (${johnBMI}) is heigher than Mark's (${markBMI})`)
 } */
+
+/* const age = 19
+
+if (age === 18) console.log("Tu viens d'être un adulte")  */
+
+
+// Coding challenge 3
+
+const noteKoala1 = 97
+const noteKoala2 = 112
+const noteKoala3 = 101
+
+const noteDauphin1 = 109
+const noteDauphin2 = 95
+const noteDauphin3 = 78
+
+const moyDauphin = (noteDauphin1 + noteDauphin2 + noteDauphin3) / 3
+console.log(moyDauphin)
+const moyKoala = (noteKoala1 + noteKoala2 + noteKoala3) / 3
+console.log(moyKoala)
+
+if (moyDauphin > moyKoala && moyDauphin >= 100) {
+    console.log("Les Dauphins ont gagné")
+} else if (moyDauphin < moyKoala && moyKoala >= 100) {
+    console.log("Les Koalas ont gagné la partie")
+} else if (moyDauphin === moyKoala && moyDauphin >= 100 && moyKoala >= 100) {
+    console.log("Les deux ont gagné")
+} else {
+    console.log("Les deux ont perdu")
+}
